a2a53cd66a3c1447c8e6fa1fb431f9bbf202e080
chunwol <chunwol@kakao.com>
2020-01-30 22:09:54 +0900

    init commit

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,6 @@ router.get('/', function(req, res, next) {
   //res.render('index', { title: 'Express' });
   res.redirect("/main");
 });
-//router.get('/main', (req, res) => res.render('main', { title: 'Express' }));
 router.get('/login', (req, res) => res.render('login', { title: 'Express' }));
 router.get('/signin', (req, res) => res.render('signin', { title: 'Express' }));
 router.get('/main', todo.main_show);
@@ -18,5 +17,9 @@ router.post('/main/del/:id', todo.main_delete);
 router.post('/main/chack/:id', todo.main_finished_update);
 router.post('/signin', todo.register);
 router.post('/login', todo.login);
+
+router.all('*', (req, res) => {
+  res.status(404).send('<h1>요청한 페이지는 없습니다.</h1>');
+});
 
 module.exports = router;
