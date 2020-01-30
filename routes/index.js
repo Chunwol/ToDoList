a4b4cf1713673bd003f17a17f898d3c9d6917c04
chunwol <chunwol@kakao.com>
2020-01-30 23:17:27 +0900

    init commit

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,7 @@ const todo = require('./todo');
 const router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  //res.render('index', { title: 'Express' });
+router.get('/', (req, res, next) => {
   res.redirect("/main");
 });
 router.get('/login', (req, res) => res.render('login', { title: 'Express' }));
