a6ee193edd8e3d52473616efaa19fd3a8271b907
Chunwol <chunwol@kakao.com>
2021-04-18 23:53:55 +0900

    server

--- app.js
+++ app.js
@@ -44,4 +44,7 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+var server = app.listen(process.env.PORT, function(){
+	console.log("Express Server has started on.")
+});
 module.exports = app;
